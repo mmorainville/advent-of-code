779f1ed8a9a2ffa9d0d950289b0fc6e7a99f5d46
Mathieu Morainville <mathieumorainville@hotmail.com>
2020-12-05 00:54:25 +0100

    feat: finish 04_2

--- 04.js
+++ 04.js
@@ -17,12 +17,64 @@ readInterface.on('line', (input) => {
   }
 });
 
-function validatePassportsOne (passports) {
-  const fieldsRequired = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
-  const fieldsOptional = ['cid']
+const fieldsRequired = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
 
-  const validPassports = passports.map(passport => {
-    return fieldsRequired.every(field => Object.keys(passport).includes(field))
+function validatePassportsOne (passports) {
+  const validPassports = passports.map(passport => fieldsRequired.every(field => Object.keys(passport).includes(field)))
+  return validPassports.filter(validPassport => validPassport).length
+}
+
+function validatePassportsTwo (passports) {
+  let validPassports = []
+  passports.filter(passport => fieldsRequired.every(field => Object.keys(passport).includes(field))).forEach(passport => {
+    let isValid = true
+
+    for (field in passport) {
+      switch (field) {
+        case 'byr':
+          isValid &= /^\d{4}$/.test(passport[field]) && (1920 <= passport[field] && passport[field] <= 2002)
+          break
+        case 'iyr':
+          isValid &= /^\d{4}$/.test(passport[field]) && (2010 <= passport[field] && passport[field] <= 2020)
+          break
+        case 'eyr':
+          isValid &= /^\d{4}$/.test(passport[field]) && (2020 <= passport[field] && passport[field] <= 2030)
+          break
+        case 'hgt':
+          let regexHeight = /^(\d*)(cm|in)$/
+          if (regexHeight.test(passport[field])) {
+            [, value, unit] = passport[field].match(regexHeight)
+            if (value && unit) {
+              switch (unit) {
+                case 'cm':
+                  isValid &= 150 <= value && value <= 193
+                  break
+                case 'in':
+                  isValid &= 59 <= value && value <= 76
+                  break
+              }
+            }
+          } else {
+            isValid &= false
+          }
+          break
+        case 'hcl':
+          isValid &= /^#([a-f0-9]{6})$/.test(passport[field])
+          break
+        case 'ecl':
+          isValid &= ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].includes(passport[field])
+          break
+        case 'pid':
+          isValid &= /^([0-9]{9})$/.test(passport[field])
+          break
+        default:
+          // ignore field
+      }
+    }
+
+    if (isValid) {
+      validPassports.push(passport)
+    }
   })
 
   return validPassports.filter(validPassport => validPassport).length
@@ -38,4 +90,5 @@ readInterface.on('close', () => {
     }, {}))
 
   console.log(validatePassportsOne(passports))
+  console.log(validatePassportsTwo(passports))
 });
